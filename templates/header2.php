34f5b8f9b207babcd220dc71078a775d761b2012
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-01 15:41:36 +0200

    test update

--- templates/header2.php
+++ templates/header2.php
@@ -1,10 +1,10 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-      <h1 class="logo"><a href="./index.php">Astra</a></h1>
+      <h1 class="logo"><a href="https://astra-bot.de">Astra</a></h1>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="./index.php">Home</a></li>
+          <li><a class="nav-link scrollto active" href="https://astra-bot.de">Home</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
