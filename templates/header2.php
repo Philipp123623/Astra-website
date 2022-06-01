7aa4fdf57010476e45f8f7e7bd5c324952ecd88e
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-01 15:37:58 +0200

    test update

--- templates/header2.php
+++ templates/header2.php
@@ -1,6 +1,6 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-      <h1 class="logo"><a href="index.php">Astra</a></h1>
+      <h1 class="logo"><a href="./index.php">Astra</a></h1>
 
       <nav id="navbar" class="navbar">
         <ul>
