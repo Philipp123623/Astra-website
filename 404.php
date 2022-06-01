7820567f23a9684ff6fb4400ab874eb3f773b812
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-01 15:51:20 +0200

    test update

--- 404.php
+++ 404.php
@@ -49,7 +49,7 @@ include_once './templates/header2.php';
 
         </div>
         <div class="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150">
-          <img src="assets/img/404.gif" class="img-fluid animated" alt="", height="250", width="250">
+          <img src="assets/img/better_404.png" class="img-fluid animated" alt="", height="250", width="250">
         </div>
       </div>
     </div>
