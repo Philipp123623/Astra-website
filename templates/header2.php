91c8ea6831aaa09ae66f6278966d40608ae84c7f
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-01 15:39:54 +0200

    test update

--- templates/header2.php
+++ templates/header2.php
@@ -4,7 +4,7 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
+          <li><a class="nav-link scrollto active" href="./index.php">Home</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
