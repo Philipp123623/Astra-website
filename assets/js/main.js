91b9c91f852c58137168c05240b07c0d719b7cf3
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-07 20:27:16 +0200

    ✨ Typo fix in Readme.md

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Techie - v4.3.0
-* Template URL: https://bootstrapmade.com/techie-free-skin-bootstrap-3/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
@@ -146,6 +140,42 @@
       scrollto(this.hash)
     }
   }, true)
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
 
   /**
    * Scroll with ofset on page load with hash links in the url
