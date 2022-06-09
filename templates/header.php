792f0f2bfc262a756842bfb69529659b84fd7433
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-09 20:20:47 +0200

    🎉 Some Deisgn updates

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,19 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-       <a href="https://astra-bot.de/"><img src="../assets/img/Astra_Stern.png" height="65" width="75" alt=""></a>
+        <a href="/" aria-label="Home redirect" class="logo d-flex align-items-center">
+            <span>Astra</span>
+        </a>
         <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li><a class="nav-link scrollto" href="#features">Features</a></li>
-          <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
-          <li><a class="file" href="commands">Commands</a></li>
-          <li><a class="getstarted" href="invite.php">Invite</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
+                <li><a class="nav-link scrollto" href="#team">Team</a></li>
+                <li><a class="nav-link scrollto" href="#features">Features</a></li>
+                <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
+                <li><a class="file" href="commands">Commands</a></li>
+                <li><a class="getstarted" href="invite.php">Invite</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
-
     </div>
 </header>
--- templates/header2.php
+++ templates/header2.php
@@ -1,13 +1,11 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
         <a href="https://astra-bot.de/"><img src="../assets/img/Astra_Stern.png" height="65" width="75" alt=""></a>
-
         <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="https://astra-bot.de">Home</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+            <ul>
+                <li><a class="nav-link scrollto active" href="https://astra-bot.de">Home</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
-
     </div>
 </header>
