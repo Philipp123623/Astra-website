514b81f7d87667add29f0078a46f73c7b7387165
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-09 20:25:08 +0200

    🎉 Some Deisgn updates

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,6 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-        <a href="/" aria-label="Home redirect" class="logo d-flex align-items-center">
-            <span>Astra</span>
-        </a>
+        <a href="https://astra-bot.de/"><img src="../assets/img/Astra_Stern.png" height="65" width="75" alt=""></a>
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
