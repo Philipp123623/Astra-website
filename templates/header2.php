1b001b0af88d9d33224fda3791b6f5b911f82eb4
Philipp Stüve <p.s.stueve@gmail.com>
2022-06-28 23:36:48 +0200

    🎉 Some Design updates

--- templates/header2.php
+++ templates/header2.php
@@ -1,6 +1,6 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center justify-content-between">
-        <a href="https://astra-bot.de/"><img src="../assets/img/Astra_Stern.png" height="65" width="75" alt=""></a>
+        <a href="https://astra-bot.de/" class="logo"><img src="../assets/img/Astra_Stern.png" alt="" ></a>
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a class="nav-link scrollto active" href="https://astra-bot.de">Home</a></li>
